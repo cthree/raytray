013b0cb644401466f0ffde9cfa65e3eb20bd12f3
cthree <cthree87@gmail.com>
2019-12-14 16:00:50 -0500

    vector and point macros

--- src/tuple.rs
+++ src/tuple.rs
@@ -1,10 +1,15 @@
-#[derive(Debug, PartialEq)]
-pub struct Float {
-    inner: f32,
+#![allow(unused_macros, dead_code)]
+
+macro_rules! point {
+    ($x:expr, $y:expr, $z:expr) => {
+        Tuple::point($x, $y, $z);
+    };
 }
 
-impl Float {
-
+macro_rules! vector {
+    ($x:expr, $y:expr, $z:expr) => {
+        Tuple::vector($x, $y, $z);
+    };
 }
 
 #[derive(PartialEq, Debug)]
@@ -43,25 +48,63 @@ impl Tuple {
     pub fn w(&self) -> f32 {
         self.w
     }
-}
 
+    pub fn is_vector(&self) -> bool {
+        self.w == 0.0
+    }
+
+    pub fn is_point(&self) -> bool {
+        self.w == 1.0
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_it_works() {
-        assert!(true);
+    fn test_w_is_0_for_a_vector() {
+        assert_eq!(Tuple::vector(0.0, 0.0, 0.0).w(), 0.0);
     }
 
     #[test]
-    fn test_a_vector_w_is_0() {
-        assert_eq!(Tuple::vector(0.0,0.0,0.0).w(), 0.0);
+    fn test_is_vector_is_true_for_vector() {
+        let test_vector = Tuple::vector(0.0, 0.0, 0.0);
+        assert!(test_vector.is_vector());
     }
 
     #[test]
-    fn test_a_point_w_is_1() {
-        assert_eq!(Tuple::point(0.0,0.0,0.0).w(), 1.0);
+    fn test_is_vector_is_false_if_point() {
+        let test_point = Tuple::point(0.0, 0.0, 0.0);
+        assert!(!test_point.is_vector());
+    }
+
+    #[test]
+    fn test_vector_macro_creates_a_vector() {
+        let test_vector = vector!(0.0, 0.0, 0.0);
+        assert!(test_vector.is_vector());
+    }
+
+    #[test]
+    fn test_w_is_1_for_a_point() {
+        assert_eq!(Tuple::point(0.0, 0.0, 0.0).w(), 1.0);
+    }
+
+    #[test]
+    fn test_is_point_is_true_if_point() {
+        let test_point = Tuple::point(0.0, 0.0, 0.0);
+        assert!(test_point.is_point());
+    }
+
+    #[test]
+    fn test_is_point_is_false_if_vector() {
+        let test_point = Tuple::vector(0.0, 0.0, 0.0);
+        assert!(!test_point.is_point());
+    }
+
+    #[test]
+    fn test_point_macro_creates_a_point() {
+        let test_point = point!(0.0, 0.0, 0.0);
+        assert!(test_point.is_point());
     }
 }
